a4ca6af440ee08a7f7cfc6e040d4c5215f50dc52
Emi Jacobsen <emilia.jacobsen@winsor.edu>
2019-11-01 19:04:46 -0400

    added a simple autonomous function that just parks the robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveAndParkAuton.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.libraries.DrivingLibrary;
+
+@Autonomous
+
+public class MoveAndParkAuton extends LinearOpMode {
+    DrivingLibrary drivingLibrary;
+    int drivingMode;
+
+    public void runOpMode() throws InterruptedException {
+        drivingLibrary = new DrivingLibrary(this);
+        drivingLibrary.setSpeed(1);
+        drivingMode = 0;
+        drivingLibrary.setMode(drivingMode);
+
+        boolean ranOnce = false;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            if (!ranOnce){
+                drivingLibrary.strafe(0,0.5f);
+                sleep(100);
+                drivingLibrary.brakeStop();
+            }
+            ranOnce = true;
+        }
+    }
+}
