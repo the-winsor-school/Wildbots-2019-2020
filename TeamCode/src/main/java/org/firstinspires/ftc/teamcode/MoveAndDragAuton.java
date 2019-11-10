70a2891d10a2c6b6d206efdb8ebf870548d5ec87
Emi Jacobsen <emilia.jacobsen@winsor.edu>
2019-11-09 19:20:50 -0500

    Worked on auton that moves tray

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveAndDragAuton.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.libraries.DrivingLibrary;
+
+@Autonomous
+
+public class MoveAndDragAuton extends LinearOpMode {
+    DrivingLibrary drivingLibrary;
+    int drivingMode;
+    Servo baseArm;
+
+    public void runOpMode() throws InterruptedException {
+        drivingLibrary = new DrivingLibrary(this);
+        drivingLibrary.setSpeed(1);
+        drivingMode = 0;
+        drivingLibrary.setMode(drivingMode);
+        boolean ranOnce = false;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            if (!ranOnce){
+                baseArm = hardwareMap.get(Servo.class, "baseArm");
+                drivingLibrary.strafe(0,0.5f); //go straight
+                sleep(700);
+                drivingLibrary.brakeStop();
+                drivingLibrary.turn(1,0);
+                sleep(500);
+
+                baseArm.setPosition(0.5);
+                drivingLibrary.strafe(0,-0.5f); //go straight
+                sleep(200);
+
+                baseArm.setPosition(-0.5);
+            }
+            ranOnce = true;
+        }
+    }
+}
