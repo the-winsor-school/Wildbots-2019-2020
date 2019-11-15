b3b1dfc37e928d4c796f44fc711322f7fbf18c69
Emi Jacobsen <emilia.jacobsen@winsor.edu>
2019-11-15 17:02:47 -0500

    hi kate sup added more encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonWithEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonWithEncoders.java
@@ -2,12 +2,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.libraries.DrivingLibrary;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 
 public class SimpleAutonWithEncoders extends LinearOpMode {
     DrivingLibrary drivingLibrary;
     int drivingMode;
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor rightRear;
+    DcMotor leftRear;
+
 
     public void runOpMode() throws InterruptedException {
         drivingLibrary = new DrivingLibrary(this);
@@ -17,8 +24,14 @@ public class SimpleAutonWithEncoders extends LinearOpMode {
         boolean ranOnce = false;
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENDODER);
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENDODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENDODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENDODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENDODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         waitForStart();
 
         while (opModeIsActive()){
