eaa297fbc60253be588d3df897dea19430141727
Emi Jacobsen <emilia.jacobsen@winsor.edu>
2019-11-16 17:01:31 -0500

    worked on encoder more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonWithEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonWithEncoders.java
@@ -24,26 +24,31 @@ public class SimpleAutonWithEncoders extends LinearOpMode {
         boolean ranOnce = false;
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        //setting mode
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        /*rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);*/
+        //setting position
         leftFront.setTargetPosition(1440);
-        rightFront.setTargetPosition(1440);
+        /*rightFront.setTargetPosition(1440);
         leftRear.setTargetPosition(1440);
         rightRear.setTargetPosition(1440);
+        waitForStart();*/
 
-        waitForStart();
-
-        while (opModeIsActive()){
+        while (leftFront.isBusy()/* && leftRear.isBusy() && rightRear.isBusy() && rightFront.isBusy() */&&opModeIsActive()){
             if (!ranOnce){
 
             }
             ranOnce = true;
         }
+        leftFront.setPower(0);
+        /*leftRear.setPower(0);
+        rightRear.setPower(0);
+        rightFront.setPower(0);*/
     }
 }
