4611ac7e44daf9beed579ffa34828dcef31af301
Emi Jacobsen <emilia.jacobsen@winsor.edu>
2020-03-05 08:18:32 -0500

    made a grabber arm test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/GrabberTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/GrabberTest.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-public class GrabberTest {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Disabled
+@Autonomous
+public class GrabberTest extends LinearOpMode {
+    Servo grabber;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        grabber = hardwareMap.get(Servo.class, "grabber");
+        boolean ranOnce = false;
+        waitForStart();
+        if (opModeIsActive()) {
+            grabber.setPosition(1);
+            sleep(1875);
+            grabber.setPosition(0.3);
+            sleep(1875);
+            grabber.setPosition(1);
+        }
+    }
 }
